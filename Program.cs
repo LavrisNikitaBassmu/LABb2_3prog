bc018919b87cd2a57d0fc5d771b07c1d76ed25e6
Admin <Admin@WIN-0QFN66U68EP>
2024-10-27 17:25:50 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int count = 0;
+        int a = 1, b = 1;
+
+        while (b < 10000)
+        {
+            if (b >= 1000)
+            {
+                count++;
+            }
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        Console.WriteLine($"Количество четырехзначных чисел в ряде Фибоначчи: {count}");
+    }
+}
